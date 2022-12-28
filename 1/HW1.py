0ef6730a4a2b665a2ce176977d57af3da5c4e672
Anastasiia <anastasiavictorivna1@gmail.com>
2022-12-28 15:39:25 +0200

    HW

---
+++ 1/HW1.py
@@ -0,0 +1,17 @@
+first=10
+second=30
+result1 = first+second
+result2 = first-second
+result3 = first*second
+result4 = first/second
+print(result1, result2, result3, result4)
+
+
+comparison_result = first == second
+print(comparison_result)
+comparison_result = first != second
+print(comparison_result)
+comparison_result = first > second
+print(comparison_result)
+comparison_result = first < second
+print(comparison_result)
